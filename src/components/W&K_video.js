2bdb0a4006be3e6fa2d2a1b635315ee3c686eada
Alyona Shunevych <olena17ju06@gmail.com>
2023-12-11 16:16:39 +0200

    Added favicon

--- src/components/W&K_video.js
+++ src/components/W&K_video.js
@@ -8,7 +8,7 @@ export default function WKVideo() {
             <div className='vid_cont'>
                 <video autoPlay muted loop src={video1} />
                 <div class="cont_main">
-                    <p className='text_on_main'>new colection</p>
+                    <p className='text_on_main'>new collection</p>
                     <p className='title_on_main'>Warm & Knitted</p>
                     <button className='discover_more'>Discover more</button>
                 </div>
