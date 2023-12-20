3fd1d82c6d81a26ab5444291b555fdd8169f10ac
Alyona Shunevych <olena17ju06@gmail.com>
2023-12-20 14:19:29 +0200

    FAQ styles have been fixed

--- src/components/pages/FAQ.js
+++ src/components/pages/FAQ.js
@@ -27,32 +27,30 @@ const FAQPage = () => {
     }, []);
 
     return (
-        
+
         <>
-        <Helmet>
-            <meta name="description" content="Ласкаво просимо в Attire, де мода поєднується з комфортом, а стиль переплітається з доступністю. У нашому онлайн-магазині ми підбираємо різноманітну колекцію одягу та аксесуарів для жінок, чоловіків і дітей, а також чудовий вибір предметів домашнього декору." />
-            <title>Attire - FAQ</title>
-        </Helmet>
-        <div className='content'>
-            <div className="delmain">
-                <h1 className='title_MA40_Faq'>Frequently Asked Questions (FAQ)</h1>
-                <ul>
-                    {faqs.map((faq) => (
-                        <div className='faq-question'>
-                        <div className='acc_butt2'>
-                        <li key={faq.id}>
-                            <NavLink to={`/faq/${faq.id}/`} className={'login_text'}>{faq.que}</NavLink>
-                            <div className='arrow_login' />
-                        </li>
-                        </div>
-                    </div>
-                    ))}
-                </ul>
-                <div className='faq-question'>
+            <Helmet>
+                <meta name="description" content="Ласкаво просимо в Attire, де мода поєднується з комфортом, а стиль переплітається з доступністю. У нашому онлайн-магазині ми підбираємо різноманітну колекцію одягу та аксесуарів для жінок, чоловіків і дітей, а також чудовий вибір предметів домашнього декору." />
+                <title>Attire - FAQ</title>
+            </Helmet>
+            <div className='content'>
+                <div className="delmain">
+                    <h1 className='title_MA40_Faq'>Frequently Asked Questions (FAQ)</h1>
+                    <ul>
+                        {faqs.map((faq) => (
+                            <div className='faq-question'>
+                                <NavLink to={`/faq/${faq.id}/`} className='acc_butt2'>
+                                    <p className={'login_text'}>{faq.que}</p>
+                                    <div className='arrow_login' />
+                                </NavLink>
+                            </div>
+                        ))}
+                    </ul>
+                    <div className='faq-question'>
                         <p className="thanks">Thank you for shopping with us!</p>
                     </div>
+                </div>
             </div>
-        </div>
         </>
     );
 };
